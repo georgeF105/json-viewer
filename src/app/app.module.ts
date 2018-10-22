da1de7d8f8aedf33fa3023d091981caf315ea426
george francis <georgefrancis105@gmail.com>
2018-10-22 19:48:52 +1300

    Show module reasons and children

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FileUploaderModule } from './file-uploader/file-uploader.module';
 import { ViewerModule } from './viewer/viewer.module';
+import { ModuleViewModule } from './module-view/module-view.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot([]),
     BrowserModule,
     FileUploaderModule,
-    ViewerModule
+    ViewerModule,
+    ModuleViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
